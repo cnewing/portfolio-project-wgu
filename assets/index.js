d0861515ca17301ce4b1707408cfacf786ccb59d
Christa Newing <christanewing@Christas-MacBook-Pro.local>
2025-04-25 16:50:12 -0400

    updated js date function and console.log for all pages

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+//JS FOR CURRENT DATE//
+
+    document.getElementById("current-date").textContent = new Date().toLocaleDateString();
+
+//JS CONSOLE LOG//
+
+console.log('Page loaded successfully!');
